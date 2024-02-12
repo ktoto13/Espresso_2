268be6a4e71ffc9339014e660137513fdb471af0
Natalia <traffkina@mail.ru>
2024-02-12 20:29:25 +0300

    correct

--- app/src/main/java/ru/kkuzmichev/simpleappforespresso/EspressoIdlingResources.java
+++ app/src/main/java/ru/kkuzmichev/simpleappforespresso/EspressoIdlingResources.java
@@ -7,4 +7,14 @@ public class EspressoIdlingResources {
     private static final String RESOURCE = "Global";
     public static CountingIdlingResource idlingResource = new CountingIdlingResource(RESOURCE);
 
+    public static void increment() {
+        idlingResource.increment();
+    }
+
+    public static void decrement() {
+        if (!idlingResource.isIdleNow()) {
+            idlingResource.decrement();
+        }
+    }
+
 }
